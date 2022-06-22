81f9d2290460b0499fb0c040f80b6201cca7ad5c
Pickachu Chen <pickachu1006@gmail.com>
2022-06-22 15:32:41 +0800

    20220622_add lucky wheel

--- js/all.js
+++ js/all.js
@@ -7,28 +7,40 @@ $(document).ready(function () {
         }, 900);
     });
 
-    $('a[href="#LINEPOINT"]').click(function (e) {
+    $('a[href="#wheel"]').click(function (e) {
         e.preventDefault();
         $('html , body').animate({
-            scrollTop: $('#LINEPOINT').offset().top,
+            scrollTop: $('#wheel').offset().top,
         }, 900);
     });
 
-    // coupon折扣
-    $('.coupon01_btn').click(function () {
-        $('#money').text(`$250`);
-        $('#coupon_money').text(`$250`);
-        $('#copy').text(`L2502206BEBN`);
+    $(".luckywheel_btn").click(function (e) {
+        e.preventDefault();
+        $('.luckywheel_wheel').rotate({
+            // angle: 0, // 旋轉指定角度(沒有動畫)
+            animateTo: 2160, // 旋轉指定角度(有動畫)
+            // duration: 1000, // 動畫持續時間
+            // bind: {}, // 綁定事件
+            // step: function () { }, // 在動畫每一步都會執行
+            callback: function(){   
+                $("#exampleModal_2").attr({
+                    style: "display: block;"
+                }); 
+                $("body").addClass("modal-open");
+                $("#exampleModal_2").addClass("show");
+                $(".luckywheel_box .luckywheel_200").attr({
+                    style: "display: block;"
+                }); 
+            }// 完成後執行
+        });
     });
-    $('.coupon02_btn').click(function () {
-        $('#money').text(`$200`);
-        $('#coupon_money').text(`$200`);
-        $('#copy').text(`L2206BE200BN`);
-    });
-    $('.coupon03_btn').click(function () {
-        $('#money').text(`$150`);
-        $('#coupon_money').text(`$150`);
-        $('#copy').text(`L2206BEBN150`);
+    $("button.wheel_close").click(function (e) { 
+        e.preventDefault();
+        $("#exampleModal_2").attr({
+            style: "display: none;"
+        }); 
+        $("body").removeClass("modal-open");
+        $("#exampleModal_2").addClass("hide");
     });
     // 點01_Xmas_btn會連到並"展開"id="Xmas"
     // $('.Xmas_btn').click(function (e) {
